6ddaf135c716a08271993d188c89985f6b604c3b
Sushmita <ssubrama@gmail.com>
2011-01-23 23:04:41 -0800

    changed images for the project index page. embedded video for projects.

--- public/javascripts/images.js
+++ public/javascripts/images.js
@@ -4,11 +4,15 @@ var placeholder = gallery.find("#placeholder img");
 
 	gallery.find("ul a").click(function() {
 
+		
     var href = this.href;
     placeholder.attr("src", href);
     //placeholder.fadeOut("fast", function() {
     //    $(this).attr("src", href).fadeIn("fast");
-    
+		
+		//$('#caption').replaceWith('<div id=' + $(this).attr("alt") + '</div>');
+		alert(this);
+  //  $('#caption').text(this.alt);
     return false;
 });
 });
