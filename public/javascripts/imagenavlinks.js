43a0a9567d18264b0f9dbf1781f0ed8bd0cdd2ec
Sushmita <ssubrama@gmail.com>
2011-02-13 19:06:55 -0800

    trying to fix image preloading

--- public/javascripts/imagenavlinks.js
+++ public/javascripts/imagenavlinks.js
@@ -1,39 +1,31 @@
 $(function(){
+	var pics = $("#frame").find("img");
+	var text = $("#nav").find("a");
 
-
-var pics = $("#frame").find("img");
-var text = $("#nav").find("a");
-
-$('.navlink').mouseenter(function(e){
-	 $.each(pics, function(i, val){
-		if(e.target.className == val.className){
-			var newsrc = $(this).attr("src").replace("bw", "color");
-			$(this).attr("src", newsrc);
-			//alert("oogabooga:in " + $(this).attr("src") + " " + text.get(i).className);
-			text.eq(i).css({'color':'#339966'});
-			break;
-		}
-		
-	 });
-
-	
-});
-
-$('.navlink').mouseleave(function(e){
-	$.each(pics, function(i, val){
-		if(e.target.className == val.className){
-			var newsrc = $(this).attr("src").replace("color", "bw");
-			$(this).attr("src", newsrc);
-			text.eq(i).css({'color':'#000'});
-			//alert("out " + $(this).attr("src"));
-		}
-
+	$('.navlink').mouseenter(function(e){
+		 $.each(pics, function(i, val){
+			if(e.target.className == val.className){
+				var newsrc = $(this).attr("src").replace("bw", "color");
+				$(this).attr("src", newsrc);
+				//alert("oogabooga:in " + $(this).attr("src") + " " + text.get(i).className);
+				text.eq(i).css({'color':'#339966'});
+				break;
+			}
+		 });
 	});
 
+	$('.navlink').mouseleave(function(e){
+		$.each(pics, function(i, val){
+			if(e.target.className == val.className){
+				var newsrc = $(this).attr("src").replace("color", "bw");
+				$(this).attr("src", newsrc);
+				text.eq(i).css({'color':'#000'});
+				//alert("out " + $(this).attr("src"));
+			}
 
-
+		});
+	});
 });
-
 
 (function($) {
 	var cache = [];
@@ -45,10 +37,7 @@ $('.navlink').mouseleave(function(e){
 			cache.push(cacheImage);
 		}
 	}
-
-
 })(jQuery)
 
-
-
-});
+jQuery.preLoadImages("home/designframe_color.png", "home/artframe_color.png",
+					"home/resumeframe_color.png", "home/aboutframe.png");
