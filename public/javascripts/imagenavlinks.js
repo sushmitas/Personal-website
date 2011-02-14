ea5b4524f0a2e0ec6b151b232fd5834720c2569f
Sushmita <ssubrama@gmail.com>
2011-02-13 19:54:21 -0800

    fixing the nav/frames linking to work in firefox

--- public/javascripts/imagenavlinks.js
+++ public/javascripts/imagenavlinks.js
@@ -1,49 +1,83 @@
 $(function(){
-	var pics = $("#frame").find("img");
-	var text = $("#nav").find("a");
-
-	$('.navlink').mouseenter(function(e){
-		 $.each(pics, function(i, val){
-			if(e.target.className == val.className){
-				var newsrc = $(this).attr("src").replace("bw", "color");
-				$(this).attr("src", newsrc);
-				//alert("oogabooga:in " + $(this).attr("src") + " " + text.get(i).className);
-				text.eq(i).css({'color':'#339966'});
-				break;
-			}
-		 });
-	});
-
-	$('.navlink').mouseleave(function(e){
-		$.each(pics, function(i, val){
-			if(e.target.className == val.className){
-				var newsrc = $(this).attr("src").replace("color", "bw");
-				$(this).attr("src", newsrc);
-				text.eq(i).css({'color':'#000'});
-				//alert("out " + $(this).attr("src"));
-			}
-
-		});
-	});
-
-
+	var pics = $.find("#frame img");
+	var textlinks = $.find("#nav a");
 	
+	//Set up picture hover event handlers
+	$.each(pics, function(){
+		//$("#debug").append($(this).attr("src") + "<br />");
+		$(this).hover(
+			//mouseenter handler
+			function(e){
+				//Output debug info to page
+				//$("#debug").append("hover " + $(this).attr("src"))
+				//Find index of hovered image
+				var index = 0;
+				while(pics[index] != this){index += 1;}
+				//$("#debug").append(" " + index)
+				//Highlight the text link
+				$("#nav a").eq(index).css({color: '#396'});
+				
+				//Change the image
+				var src = $(this).attr("src").replace("bw", "color");
+				$(this).attr("src", src);
+			}, 
+			//mouseleave handler
+			function(e){
+				//Output debug info to page
+				//$("#debug").append(" out<br />")
+				
+				//Find index of hovered image
+				var index = 0;
+				while(pics[index] != this){index += 1;}
+				//$("#debug").append(" " + index)
+				//Highlight the text link
+				$("#nav a").eq(index).css({color: '#000'});
+				
+				//Change the image
+				var src = $(this).attr("src").replace("color", "bw");
+				$(this).attr("src", src);
+			}
+		);
+	
+	});
+	
+	//Set up text link hover event handlers
+	$.each(textlinks, function(){
+		//$("#debug").append($(this).attr("src") + "<br />");
+		$(this).hover(
+			function(e){
+				$("#debug").append("hover " + $(this).attr("href"))
+				//Highlight the text link
+				$(this).css({color: '#396'});
+				
+				//Find index of hovered link
+				var index = 0;
+				while(textlinks[index] != this){index += 1;}
+				//$("#debug").append(" " + index)
+				//Change the image
+				var src = $("#frame img").eq(index).attr("src").replace("bw", "color");
+				$("#frame img").eq(index).attr("src", src);
+			}, 
+			function(e){
+				//$("#debug").append(" out<br />")
+				//Highlight the text link
+				$(this).css({color: '#000'});
+				
+				//Find index of hovered link
+				var index = 0;
+				while(textlinks[index] != this){index += 1;}
+				//$("#debug").append(" " + index)
+				//Change the image
+				var src = $("#frame img").eq(index).attr("src").replace("color", "bw");
+				$("#frame img").eq(index).attr("src", src);
+			}
+		);
+	
+	});
+
+/*pics.hover(function(e){
+	$("#debug").append($(this).attr("src") + " hovered<br />");
 });
+*/
 
-//(function($) {
-//	var cache = [];
-//	alert('in cache function');
-//	$.preLoadImages = function(){
-//		alert('in preload image function');
-//		var args_len = arguments.length;
-//		for(var i = args_len; i--;){
-//			var cacheImage = document.createElement('img');
-//			cacheImage.src = arguments[i];
-//			cache.push(cacheImage);
-//		}
-//	}
-//})(jQuery)
-
-
-//jQuery.preLoadImages("home/designframe_color.png", "home/artframe_color.png",
-//					"home/resumeframe_color.png", "home/aboutframe.png");
+});
