f65ff71b787e6e4d08948bd5cdfc9f294bc58cba
Sushmita <ssubrama@gmail.com>
2011-02-13 17:50:36 -0800

    linked the nav links and the framed pictures on the homepage

---
+++ public/javascripts/imagenavlinks.js
@@ -0,0 +1,41 @@
+$(function(){
+var pics = $("#frame").find("img");
+var text = $("#nav").find("a");
+
+
+
+$('.navlink').mouseenter(function(e){
+	 $.each(pics, function(i, val){
+		if(e.target.className == val.className){
+			var newsrc = $(this).attr("src").replace("bw", "color");
+			$(this).attr("src", newsrc);
+			//alert("oogabooga:in " + $(this).attr("src") + " " + text.get(i).className);
+			text.eq(i).css({'color':'#339966'});
+			break;
+		}
+		
+	 });
+
+	
+});
+
+$('.navlink').mouseleave(function(e){
+	$.each(pics, function(i, val){
+		if(e.target.className == val.className){
+			var newsrc = $(this).attr("src").replace("color", "bw");
+			$(this).attr("src", newsrc);
+			text.eq(i).css({'color':'#000'});
+			//alert("out " + $(this).attr("src"));
+		}
+
+	});
+
+
+
+});
+
+
+
+
+
+});
